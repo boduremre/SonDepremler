721adca46cd89e6e3445110d932bf05e945c81d9
Emre Bodur <emrebodurbto@gmail.com>
2022-11-28 10:32:11 +0300

    Proje dosyası ekle.

---
+++ SonDepremler/Controllers/DepremController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SonDepremler.Models;
+using SonDepremler.Models.ViewModels;
+using System.Diagnostics;
+
+namespace SonDepremler.Controllers
+{
+    public class DepremController : Controller
+    {
+        private readonly ILogger<DepremController> _logger;
+
+        public DepremController(ILogger<DepremController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Deprem deprem = new Deprem();
+            ViewBag.SonDepremler = deprem.Get(0);
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SonDepremler/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SonDepremler.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
